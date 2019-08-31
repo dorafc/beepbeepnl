09dbee377ad5df97feaaf617dce08ae25aae8a07
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-08-31 13:20:35 -0400

    add authentication

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,6 @@
 import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -8,9 +10,25 @@ const config = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
+
 class Firebase {
   constructor() {
+    // create connection
     app.initializeApp(config);
+
+    // authentication
+    this.auth = app.auth();
+    this.provider = new app.auth.GoogleAuthProvider()
+  }
+
+  // *** Auth API ***
+
+  doSignOut = () => {
+    this.auth.signOut();
+  }
+
+  doSignInWithRedirect = () => {
+    this.auth.signInWithRedirect(this.provider)
   }
 }
 export default Firebase;
