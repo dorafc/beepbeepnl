bedff8298bf0e3b9f030d3d0607ed7f8337f54cd
Thomas A Caswell <tcaswell@gmail.com>
2019-09-01 19:11:39 -0400

    Format existing functions

--- functions/index.js
+++ functions/index.js
@@ -5,29 +5,29 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.init_user = functions
-    .region('us-east1')
-    .auth
-    .user()
-    .onCreate(async (user, context) => {
-  // Get the uid and display name of the newly created user.
-  var uid = user.uid;
-  var displayName = user.displayName;
-        const writeResult = await admin
-              .firestore()
-              .collection('users')
-              .doc(uid)
-              .set({'name': displayName})
+  .region('us-east1')
+  .auth
+  .user()
+  .onCreate(async (user, context) => {
+    // Get the uid and display name of the newly created user.
+    var uid = user.uid;
+    var displayName = user.displayName;
+    const writeResult = await admin
+      .firestore()
+      .collection('users')
+      .doc(uid)
+      .set({ 'name': displayName })
 
-});
+  });
 
 exports.removeUserFromDatabase = functions
-    .region('us-east1')
-    .auth
-    .user()
-    .onDelete(function(user, context) {
-  // Get the uid of the deleted user.
-  var uid = user.uid;
+  .region('us-east1')
+  .auth
+  .user()
+  .onDelete(function (user, context) {
+    // Get the uid of the deleted user.
+    var uid = user.uid;
 
-  // Remove the user from your Realtime Database's /users node.
-  return admin.database().ref("/users/" + uid).remove();
-});
+    // Remove the user from your Realtime Database's /users node.
+    return admin.database().ref("/users/" + uid).remove();
+  });
