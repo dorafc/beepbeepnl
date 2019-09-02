ea369e1089564c9f976c45d3bf586b50c77e8662
Thomas A Caswell <tcaswell@gmail.com>
2019-09-01 20:34:34 -0400

    ENH: first attempt at clearing old rides

--- functions/index.js
+++ functions/index.js
@@ -36,3 +36,30 @@ exports.removeUserFromDatabase = functions
       .delete()
     return result
   });
+
+exports.clearOldRides = functions
+  .region('us-east1')
+  .firestore
+  .document('rides/{ride}').onCreate(async (new_ride, context) => {
+    await admin
+      .firestore
+      .collection('rides')
+      .where('driver',
+        '==',
+        new_ride.get('driver')
+      ).get()
+      .then((qs) => {
+        qs.forEach(async (doc) => {
+          const id = doc.id;
+          if (id === new_ride.id) {
+            return new_ride
+          }
+          else if (doc.get('toLab') == new_ride.get('toLab')) {
+            const old_data = doc.data()
+            await admin.firestore.collection('rides').doc(id).delete()
+            return old_data
+          }
+        })
+      })
+  }
+  )
